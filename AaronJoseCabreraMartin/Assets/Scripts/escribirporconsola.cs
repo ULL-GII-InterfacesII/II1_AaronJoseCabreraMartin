48a56daf0b02ec68205d29b2aed4d43c3e360f17
AaronJoseCabreraMartin <alu0101101019@ull.edu.es>
2020-10-15 16:07:36 +0100

    Terreno+Arboles+Textura

---
+++ AaronJoseCabreraMartin/Assets/Scripts/escribirporconsola.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class escribirporconsola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0.1f, 0, 0);
+    }
+}
